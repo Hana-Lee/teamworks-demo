b70d3180d1a9ab878adc5a000e653717f1e39ba3
Hana Lee <i@leehana.co.kr>
2018-06-07 17:07:51 +0900

    툴바 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <AppBar position="fixed" color="primary">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              TeamWorks
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
